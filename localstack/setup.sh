14996c4dcbfe21dfa7792a806a75e8a5afa9694f
Macy Li <macy.li@qantas.com.au>
2021-10-05 11:23:03 +1100

    add setup.sh

---
+++ localstack/setup.sh
@@ -0,0 +1,34 @@
+# aws command to create resource in localstack
+
+# create bucket 
+aws s3 mb s3://macy-localstack-demo --endpoint-url=http://localhost:4566
+
+echo 'hello_world'> hello_world.txt
+
+# upload a file 
+aws s3 cp hello_world.txt s3://macy-localstack-demo --endpoint-url=http://localhost:4566
+
+aws s3 ls s3://macy-localstack-demo --endpoint-url=http://localhost:4566
+
+# create sqs queue  in localstack
+aws sqs create-queue --queue-name my-queue --endpoint-url=http://localhost:4566
+
+aws sqs list-queues  --endpoint-url=http://localhost:4566
+
+# send message to queue 
+aws sqs send-message --queue-url=http://localhost:4566/000000000000/my-queue \
+--message-body  '{"filename": "file1" , "value" :[2 ,3, 4] }'  --endpoint-url=http://localhost:4566
+
+
+cd demo_localstack/app/; zip function.zip lambda.py
+
+# TO DO: test create lambda function
+aws lambda create-function --function-name test_lambda --runtime python3.8 \
+--role arn:aws:iam::000000000000:role/service-role/demoLambda-role-4y0nwuvw \
+--zip-file=fileb://function.zip --handler lambda.lambda_handler  --endpoint-url=http://localhost:4566 \
+--environment Variables="{LOCALSTACK=False}"
+
+
+# TO DO: test lambda invoke 
+aws lambda invoke --function-name arn:aws:lambda:ap-southeast-2:000000000000:function:test_lambda \
+ --endpoint-url=http://localhost:4566  response.json
